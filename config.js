a1b7435f01344c4a1625052b1f5bcabf9d2aa9ce
dinhhuugiac2002 <90848664+dinhhuugiac2002@users.noreply.github.com>
2021-09-16 20:50:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Vợ Trịnh Nhã Linh ơiiiiiiiii❤️❤️❤️❤️!',
+    introDesc: `Luuu bé yêu ❤️❤️❤️, Lại là chồng Đinh Hữu Giác của em đây❤️❤️`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'Anh hạnh phúc khi được gặp và được yêu bé nhiều như ngày hôm nay  😙',
+    desc: 'Em hứa sẽ không giân anh nữa nha❤️  ',
+    btnYes: 'Dạ , em hứa <33',
+    btnNo: 'Không, Đừng có mơ :3',
+    question:'Vợ hứa sẽ mãi yêu anh nha bé❤️❤️❤️  <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Em yêu anh ❤️',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Anh yêu Công Chúa Nhã Linh❤️ Xinh Đẹp Lắp Lánh Kim Tuyến Hoa Hướng Dương Sáng Tỏ Cuộc Đời Anh.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'file:///C:/Users/DINH%20HUU%20GIAC/Downloads/iuuuuu-%C4%91%C3%A3-chuy%E1%BB%83n-%C4%91%E1%BB%95i.pdf' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
